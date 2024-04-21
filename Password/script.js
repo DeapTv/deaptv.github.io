ecac0a6bbe1e776643a4d7d494c2421b18422fdd
DeapTv <167103940+DeapTv@users.noreply.github.com>
2024-04-21 15:00:06 +0300

    Add files via upload

---
+++ Password/script.js
@@ -0,0 +1,21 @@
+function validatePassword(event) {
+    event.preventDefault(); // Formun submit işlemini engelle
+
+    var passwordInput = document.getElementById('password');
+    var passwordError = document.getElementById('password-error');
+
+    // Girilen şifreyi al
+    var password = passwordInput.value;
+
+    // Doğru şifre "12345" mi kontrol et
+    if (password !== 'hayvan') {
+        passwordError.textContent = 'Hatalı şifre. Lütfen tekrar deneyin.'; // Hata mesajını göster
+        passwordInput.focus(); // Şifre alanına odaklan
+    } else {
+        passwordError.textContent = ''; // Hata mesajını temizle
+        // Burada normal giriş işlemini yapabilirsiniz
+        alert('Giriş başarılı!'); // Örneğin, bir iletişim kutusu göster
+        // Şifre doğruysa yapılacak işlemleri burada gerçekleştirin
+        window.location.href = '/deepFvD/index.html'; // Giriş başarılı olduğunda yönlendirme yap
+    }
+}
